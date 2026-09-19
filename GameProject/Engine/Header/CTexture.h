6d4a0b34ccc4d7ee46698e9157b75723ea6c3d0a
sckim13 <ksc3856571@gmail.com>
2026-09-19 23:05:39 +0900

    컴파일러 경고

--- GameProject/Engine/Header/CTexture.h
+++ GameProject/Engine/Header/CTexture.h
@@ -18,7 +18,7 @@ public:
 
 	void		Set_Texture(const _uint& iIndex = 0);
 
-	inline _int GetCount() { return m_vecTexture.size(); }
+	inline _int GetCount() { return (_int)m_vecTexture.size(); }
 
 private:
 	vector<IDirect3DBaseTexture9*>		m_vecTexture;
