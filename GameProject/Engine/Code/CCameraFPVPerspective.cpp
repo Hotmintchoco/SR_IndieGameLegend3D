78ea4c8081c35c04164a023b0be72d1b8d325a46
sckim13 <ksc3856571@gmail.com>
2026-09-20 01:54:53 +0900

    카메라 위치 조정

--- GameProject/Engine/Code/CCameraFPVPerspective.cpp
+++ GameProject/Engine/Code/CCameraFPVPerspective.cpp
@@ -15,7 +15,7 @@ CCameraFPVPerspective::~CCameraFPVPerspective()
 
 HRESULT CCameraFPVPerspective::Ready_Camera()
 {
-	m_vEye = { 0.f, 0.f, -1.f };
+	m_vEye = { 0.f, 0.f, 0.f };
 	m_vAt = { 0.f, 0.f, 0.f };
 	m_vUp = { 0.f, 1.f, 0.f };
 	m_fFov = D3DXToRadian(60.f);
@@ -40,8 +40,8 @@ void CCameraFPVPerspective::Update_Camera(const _float& fTimeDelta, const _vec3&
 	D3DXMatrixRotationAxis(&matAxis, &vRight, D3DXToRadian(m_fAngle));
 	D3DXVec3TransformNormal(&vLook, &vLook, &matAxis);
 
-	m_vEye = vPos - [&]()->_vec3 {_vec3 vLookDst;  D3DXVec3Normalize(&vLookDst, &vLook); return vLookDst * 0.5f; }();
-	m_vAt = vPos + [&]()->_vec3 {_vec3 vLookDst;  D3DXVec3Normalize(&vLookDst, &vLook); return vLookDst * 5.0f; }();
+	m_vEye = vPos;
+	m_vAt = vPos + vLook;
 
 	D3DXMatrixLookAtLH(&m_matView, &m_vEye, &m_vAt, &m_vUp);
 	m_pGraphicDev->SetTransform(D3DTS_VIEW, &m_matView);
