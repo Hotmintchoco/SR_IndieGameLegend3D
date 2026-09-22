ff23f04740f23fe8311abd1e635d1dc777b59aca
sckim13 <ksc3856571@gmail.com>
2026-09-22 23:56:05 +0900

    연쇄 폭발 merge conflict 수정

--- GameProject/Client/Code/CFrustum.cpp
+++ GameProject/Client/Code/CFrustum.cpp
@@ -1,4 +1,4 @@
-ï»¿#include "pch.h"
+#include "pch.h"
 #include "CFrustum.h"
 #include "CProtoMgr.h"
 #include "CRenderer.h"
@@ -20,7 +20,7 @@ HRESULT CFrustum::Ready_GameObject()
     if (FAILED(Add_Component()))
         return E_FAIL;
 
-    /* Note : ìˆœì„œì— ì£¼ì˜ (PostInitalizeë¡œ ë¹¼ëŠ” ê²ƒë„ ê³ ë ¤) */
+    /* Note : ¼ø¼­¿¡ ÁÖÀÇ (PostInitalize·Î »©´Â °Íµµ °í·Á) */
     if (FAILED(CGameObject::Ready_GameObject()))
         return E_FAIL;
 
@@ -88,7 +88,7 @@ _bool CFrustum::CheckDestroyCondition(CCollider* pOtherCollider)
     else
         return false;
 
-    if (ColliderID == COLL_PBULLET || ColliderID == COLL_MBULLET)
+    if (ColliderID == COLL_PBULLET || ColliderID == COLL_MBULLET || ColliderID == COLL_EXPLODERANGE)
     {
         return true;
     }
